72053af9466a1408e15de5eceeab1a81c5aab403
Ewan Stott <ewan.stott1@gmail.com>
2023-12-11 15:52:49 +0000

    spinner added

--- entry.js
+++ entry.js
@@ -1,16 +1,28 @@
 const rootRef = document.getElementById("root");
-// import axios from "axios";
+const searchRef = document.getElementById("search");
 
-async function getApiData() {
-  const result1 = await axios.get(
-    `https://thesimpsonsquoteapi.glitch.me/quotes?character=homer simpson`
+const spinner = `<span class="loader"></span>`;
+
+async function getApiData(value) {
+  //begin spinner here
+  rootRef.innerHTML = spinner;
+
+  const result = await axios.get(
+    `https://thesimpsonsquoteapi.glitch.me/quotes?character=${value}`
   );
-  const result2 = await axios.get(
-    `https://thesimpsonsquoteapi.glitch.me/quotes?character=bart simpson`
-  );
-  const result3 = await axios.get(
-    `https://thesimpsonsquoteapi.glitch.me/quotes?character=lisa simpson`
-  );
-  console.log(result1, result2, result3);
+
+  const { character, image, quote } = result.data[0];
+
+  rootRef.innerHTML = `<div>
+                                    <h1>${character}</h1>
+                                    <p>${quote}</p>
+                                    <img src="${image}" alt="${character}"/>
+                                    
+                        </div>`;
 }
-getApiData();
+
+searchRef.addEventListener("input", (e) => {
+  getApiData(e.target.value);
+});
+
+//vpn or change security settings / airvpn
